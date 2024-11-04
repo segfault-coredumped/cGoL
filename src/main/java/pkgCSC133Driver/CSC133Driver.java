50d9a84168175d1bd6b17986ac7011d3e97b7779
segfault-coredumped <146154806+segfault-coredumped@users.noreply.github.com>
2024-11-04 15:36:34 -0800

    sqArray works 16x16 and 100x100 | edges are maintained

--- src/main/java/pkgCSC133Driver/CSC133Driver.java
+++ src/main/java/pkgCSC133Driver/CSC133Driver.java
@@ -10,11 +10,11 @@ public class CSC133Driver {
     public static void main(String[] my_args) {
         // instance of pingpong manager
         PingPongManager pp = new PingPongManager(BOARDSIZE,BOARDSIZE);
-        pp.showLiveArr();
-        pp.fillNNNNextArr();
-        pp.swapArr();
-        System.out.println();
-        pp.showLiveArr();
+        //pp.showLiveArr();
+        //pp.fillNNNNextArr();
+        //pp.swapArr();
+        //System.out.println();
+        //pp.showLiveArr();
         // instance of renderer
         GoLRenderer my_re = new GoLRenderer();
         SlWindowManager.get().initGLFWWindow(WIN_WIDTH, WIN_HEIGHT, "CSUS CSC133");
