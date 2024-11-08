349165f9511af63266a3816e0f5f7c280c8ca7ae
segfault-coredumped <146154806+segfault-coredumped@users.noreply.github.com>
2024-11-07 20:22:13 -0800

    Testing Done | set to good presets

--- src/main/java/pkgCSC133Driver/SlSpot.java
+++ src/main/java/pkgCSC133Driver/SlSpot.java
@@ -1,10 +1,10 @@
 package pkgCSC133Driver;
 
 public class SlSpot {
-    public static int WIN_WIDTH = 800;
-    public static int WIN_HEIGHT = 800;
+    public static int WIN_WIDTH = 1300;
+    public static int WIN_HEIGHT = 1300;
 
-    public static int BOARDSIZE = 100;
+    public static int BOARDSIZE = 350;
 
     public static int spot_frame_delay = 1050;
 
